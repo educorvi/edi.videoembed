f46c9247473866472d2b2be207a5146fdf66a6cf
Luca Pisani <luca.pisani@abstract.it>
2017-02-02 15:29:31 +0100

    Webcode added, Field configuration

--- src/edi/videoembed/interfaces.py
+++ src/edi/videoembed/interfaces.py
@@ -1,24 +1,47 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
-
+import random
 from edi.videoembed import _
+from five import grok
 from zope import schema
 from zope.interface import Interface
+from DateTime import DateTime
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from zope.schema.interfaces import IContextAwareDefaultFactory
+from Products.CMFCore.utils import getToolByName
 from plone.namedfile.field import NamedBlobImage
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from plone.app.textfield import RichText
-
+from plone.supermodel import directives
 
 formatVocabulary = SimpleVocabulary.fromItems((
     (u"4:3", "embed-responsive-4by3"),
     (u"16:9", "embed-responsive-16by9")))
 
+@grok.provider(IContextAwareDefaultFactory)
+def genWebcode(context):
+    aktuell=unicode(DateTime()).split(' ')[0]
+    neujahr='%s/01/01' %str(DateTime()).split(' ')[0][:4]
+    konstante=unicode(aktuell[2:4])
+    zufallszahl=unicode(random.randint(100000, 999999))
+    code=konstante+zufallszahl
+    pcat=getToolByName(context,'portal_catalog')
+    results = pcat(Webcode=code, created={"query":[neujahr,aktuell],"range":"minmax"})
+    while results:
+        zufallszahl=unicode(random.randint(100000, 999999))
+        code=konstante+zufallszahl
+        results = pcat(Webcode=code, created={"query":[neujahr,aktuell],"range":"minmax"})
+    return code
+
 class IEdiVideoembedLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
 
-
 class IVideoembed(Interface):
+
+    directives.fieldset('settings',
+                        label=u'Einstellungen',
+                        fields=(u'videoformat',u'showtitle',u'showdescription',),
+    )
 
     title = schema.TextLine(
         title=u'Titel',
@@ -61,9 +84,18 @@ class IVideoembed(Interface):
 
     showtitle = schema.Bool(title = u'Titel anzeigen',
             description = u"Hier klicken wenn der Titel in der Einzeldarstellung angezeigt werden soll.",
+            default = True,
             required = False)
 
     showdescription = schema.Bool(title = u'Beschreibung anzeigen',
             description = u"Hier klicken wenn die Beschreibung in der Einzeldarstellung angezeigt werden soll.",
+            default = True,
             required = False)
 
+    webcode = schema.TextLine(
+              title=u"Webcode",
+              description=u"Der Webcode für diesen Artikel wird automatisch errechnet und angezeigt. Sie\
+                          können diesen Webcode bei Bedarf jedoch jederzeit überschreiben.",
+              required = True,
+              defaultFactory = genWebcode,
+              )
